8eaab97e9dc0e732c119efe7adfe2ce0816bc0a3
Antonio Bertucci <archano@gmail.com>
2017-03-18 20:12:57 +0100

    Extract abstract class for tasks collecting violations

---
+++ plugin/src/main/groovy/com/novoda/staticanalysis/internal/CollectViolationsTask.groovy
@@ -0,0 +1,27 @@
+package com.novoda.staticanalysis.internal
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+abstract class CollectViolationsTask extends DefaultTask {
+
+    private File xmlReportFile
+    private Violations violations
+
+    void setXmlReportFile(File xmlReportFile) {
+        this.xmlReportFile = xmlReportFile
+    }
+
+    void setViolations(Violations violations) {
+        this.violations = violations
+    }
+
+    @TaskAction
+    void run() {
+        if (xmlReportFile?.exists()) {
+            collectViolations(xmlReportFile, violations)
+        }
+    }
+
+    abstract void collectViolations(File xmlReportFile, Violations violations)
+}
--- plugin/src/main/groovy/com/novoda/staticanalysis/internal/checkstyle/CollectCheckstyleViolationsTask.groovy
+++ plugin/src/main/groovy/com/novoda/staticanalysis/internal/checkstyle/CollectCheckstyleViolationsTask.groovy
@@ -1,25 +1,18 @@
 package com.novoda.staticanalysis.internal.checkstyle
 
+import com.novoda.staticanalysis.internal.CollectViolationsTask
 import com.novoda.staticanalysis.internal.Violations
 import groovy.util.slurpersupport.GPathResult
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
 
-class CollectCheckstyleViolationsTask extends DefaultTask {
+class CollectCheckstyleViolationsTask extends CollectViolationsTask {
 
-    File xmlReportFile
-
-    Violations violations
-
-    @TaskAction
-    void run() {
-        if (xmlReportFile?.exists()) {
-            File htmlReportFile = new File(xmlReportFile.absolutePath - '.xml' + '.html')
-            GPathResult xml = new XmlSlurper().parse(xmlReportFile)
-            int errors = xml.'**'.findAll { node -> node.name() == 'error' && node.@severity == 'error' }.size()
-            int warnings = xml.'**'.findAll { node -> node.name() == 'error' && node.@severity == 'warning' }.size()
-            violations.addViolations(errors, warnings, htmlReportFile ?: xmlReportFile)
-        }
+    @Override
+    void collectViolations(File xmlReportFile, Violations violations) {
+        File htmlReportFile = new File(xmlReportFile.absolutePath - '.xml' + '.html')
+        GPathResult xml = new XmlSlurper().parse(xmlReportFile)
+        int errors = xml.'**'.findAll { node -> node.name() == 'error' && node.@severity == 'error' }.size()
+        int warnings = xml.'**'.findAll { node -> node.name() == 'error' && node.@severity == 'warning' }.size()
+        violations.addViolations(errors, warnings, htmlReportFile ?: xmlReportFile)
     }
 
 }
