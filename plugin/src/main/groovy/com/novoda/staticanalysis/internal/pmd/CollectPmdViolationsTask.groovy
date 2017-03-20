64f701945f01ac5fc5fe1b81df8951eb8de85556
Antonio Bertucci <archano@gmail.com>
2017-03-20 19:06:30 +0100

    Split variable declaration over separate lines

--- plugin/src/main/groovy/com/novoda/staticanalysis/internal/pmd/CollectPmdViolationsTask.groovy
+++ plugin/src/main/groovy/com/novoda/staticanalysis/internal/pmd/CollectPmdViolationsTask.groovy
@@ -8,7 +8,8 @@ class CollectPmdViolationsTask extends CollectViolationsTask {
     @Override
     void collectViolations(File xmlReportFile, File htmlReportFile, Violations violations) {
         PmdViolationsEvaluator evaluator = new PmdViolationsEvaluator(xmlReportFile)
-        int errors = 0, warnings = 0
+        int errors = 0
+        int warnings = 0
         evaluator.collectViolations().each { PmdViolationsEvaluator.PmdViolation violation ->
             if (violation.isError()) {
                 errors += 1
