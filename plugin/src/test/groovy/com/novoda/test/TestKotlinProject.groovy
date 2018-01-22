ee20ebf616358e64ea06e5af4b6d6db5b7d82155
Tobias Heine <tobias@novoda.com>
2018-01-22 14:19:27 +0100

    Add TestKotlinProject to setup a test kotlin project

---
+++ plugin/src/test/groovy/com/novoda/test/TestKotlinProject.groovy
@@ -0,0 +1,37 @@
+package com.novoda.test
+
+final class TestKotlinProject extends TestProject<TestKotlinProject> {
+
+    private static final Closure<String> TEMPLATE = { TestProject project ->
+        """
+plugins {
+    id 'com.novoda.static-analysis'
+}
+repositories {
+    jcenter()
+}
+apply plugin: 'kotlin'
+sourceSets {
+    ${formatSourceSets(project)}
+}
+${formatExtension(project)}
+"""
+    }
+
+    TestKotlinProject() {
+        super(TEMPLATE)
+    }
+
+    private static String formatSourceSets(TestProject project) {
+        project.sourceSets
+                .entrySet()
+                .collect { Map.Entry<String, List<String>> entry ->
+            """$entry.key {
+        kotlin {
+            ${entry.value.collect { "srcDir '$it'" }.join('\n\t\t\t\t')}
+        }
+    }"""
+        }
+        .join('\n\t')
+    }
+}
