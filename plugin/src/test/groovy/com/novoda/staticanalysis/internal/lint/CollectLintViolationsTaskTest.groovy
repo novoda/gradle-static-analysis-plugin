c705c2d2c0280f64cbd653c7473324460b7dfa13
Tobias Heine <tobias@novoda.com>
2018-01-30 17:01:31 +0100

    Add static import for Truth.assertThat

--- plugin/src/test/groovy/com/novoda/staticanalysis/internal/lint/CollectLintViolationsTaskTest.groovy
+++ plugin/src/test/groovy/com/novoda/staticanalysis/internal/lint/CollectLintViolationsTaskTest.groovy
@@ -1,11 +1,12 @@
 package com.novoda.staticanalysis.internal.lint
 
-import com.google.common.truth.Truth
 import com.novoda.staticanalysis.internal.Violations
 import com.novoda.test.Fixtures
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
+
+import static com.google.common.truth.Truth.assertThat
 
 class CollectLintViolationsTaskTest {
 
@@ -17,7 +18,7 @@ class CollectLintViolationsTaskTest {
         Violations violations = new Violations("Android Lint")
         task.collectViolations(Fixtures.Lint.SAMPLE_REPORT, null, violations)
 
-        Truth.assertThat(violations.getErrors()).isEqualTo(1)
-        Truth.assertThat(violations.getWarnings()).isEqualTo(1)
+        assertThat(violations.getErrors()).isEqualTo(1)
+        assertThat(violations.getWarnings()).isEqualTo(1)
     }
 }
