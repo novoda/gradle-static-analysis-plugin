0850ee417116a9219a4c88634bff2ba9a1bd9cb3
Tobias Heine <tobias@novoda.com>
2018-01-30 17:00:18 +0100

    Parse lint xml result and append errors and warnings to Violations

--- plugin/src/main/groovy/com/novoda/staticanalysis/internal/lint/CollectLintViolationsTask.groovy
+++ plugin/src/main/groovy/com/novoda/staticanalysis/internal/lint/CollectLintViolationsTask.groovy
@@ -2,12 +2,16 @@ package com.novoda.staticanalysis.internal.lint
 
 import com.novoda.staticanalysis.internal.CollectViolationsTask
 import com.novoda.staticanalysis.internal.Violations
+import groovy.util.slurpersupport.GPathResult
 
 class CollectLintViolationsTask extends CollectViolationsTask {
 
     @Override
     void collectViolations(File xmlReportFile, File htmlReportFile, Violations violations) {
-        //TODO: collect lint violations
+        GPathResult xml = new XmlSlurper().parse(xmlReportFile)
+        int errors = xml.'**'.findAll { node -> node.name() == 'issue' && node.@severity == 'Error' }.size()
+        int warnings = xml.'**'.findAll { node -> node.name() == 'issue' && node.@severity == 'Warning' }.size()
+        violations.addViolations(errors, warnings, htmlReportFile ?: xmlReportFile)
     }
 
 }
