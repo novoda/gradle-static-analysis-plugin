4ff93028ed5302a4af0691c175e885df80e7db15
Tobias Heine <tobias@novoda.com>
2018-01-30 13:24:42 +0100

    Remove not needed TestRule since we android lint just supports android projects.

--- plugin/src/test/groovy/com/novoda/staticanalysis/internal/lint/LintIntegrationTest.groovy
+++ plugin/src/test/groovy/com/novoda/staticanalysis/internal/lint/LintIntegrationTest.groovy
@@ -1,40 +1,24 @@
 package com.novoda.staticanalysis.internal.lint
 
 import com.novoda.test.Fixtures
+import com.novoda.test.TestAndroidProject
 import com.novoda.test.TestProject
-import com.novoda.test.TestProjectRule
-import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
 
 import static com.novoda.test.LogsSubject.assertThat
 
-@RunWith(Parameterized.class)
 class LintIntegrationTest {
-
-    @Parameterized.Parameters(name = "{0}")
-    static Iterable<TestProjectRule> rules() {
-        return [TestProjectRule.forAndroidProject()]
-    }
-
-    @Rule
-    public final TestProjectRule projectRule
-
-    LintIntegrationTest(TestProjectRule projectRule) {
-        this.projectRule = projectRule
-    }
 
     @Test
     void shouldFailBuildWhenLintWarningsOverTheThreshold() throws Exception {
-        def result = createProjectWith(Fixtures.Lint.SOURCES_WITH_WARNINGS, 0, 0)
+        def result = createAndroidProjectWith(Fixtures.Lint.SOURCES_WITH_WARNINGS, 0, 0)
                 .buildAndFail('check')
 
         assertThat(result.logs).containsLintViolations(0, 1)
     }
 
-    private TestProject createProjectWith(File sources, int maxWarnings = 0, int maxErrors = 0) {
-        def testProject = projectRule.newProject()
+    private static TestProject createAndroidProjectWith(File sources, int maxWarnings = 0, int maxErrors = 0) {
+        def testProject = new TestAndroidProject()
                 .withSourceSet('main', sources)
                 .withPenalty("""{
                     maxWarnings = ${maxWarnings}
