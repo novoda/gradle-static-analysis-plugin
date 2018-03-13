7a46c177eafd0dffd18defa8af6486706d2c0ece
takecare <rui@vazteixeira.org>
2018-03-13 17:39:24 +0000

    add some "errors"

--- sample/app/src/main/java/com/novoda/buildpropertiesplugin/sample/MyActivity.kt
+++ sample/app/src/main/java/com/novoda/buildpropertiesplugin/sample/MyActivity.kt
@@ -3,6 +3,7 @@ package com.novoda.buildpropertiesplugin.sample
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_my.*
 
 class MyActivity : AppCompatActivity() {
@@ -11,6 +12,11 @@ class MyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
 
-        button.setOnClickListener({ startActivity(Intent(this, SomeOtherActivity::class.java)) })
+        button.setOnClickListener({
+            LOOK_ANOTHER_CRAZY_LONG_LONG_LONG_METHOD_NAME_FOR_NO_APPARENT_REASON_WHY_IS_THIS_METHOD_NAMED_LIKE_THIS_WHY_IT_LITERALLY_MAKES_NO_SENSE_WHATSOEVER(1)
+            startActivity(Intent(this, SomeOtherActivity::class.java))
+        })
     }
+
+    private fun LOOK_ANOTHER_CRAZY_LONG_LONG_LONG_METHOD_NAME_FOR_NO_APPARENT_REASON_WHY_IS_THIS_METHOD_NAMED_LIKE_THIS_WHY_IT_LITERALLY_MAKES_NO_SENSE_WHATSOEVER(duration: Int) = Toast.makeText(this, "some useless message", duration).show()
 }
