c3c959fc9a97c6186b644395c1dfa510512a9b3a
Said Tahsin Dane <tasomaniac@gmail.com>
2018-08-13 15:43:57 +0300

    Add version to incompatibility error

--- plugin/src/main/groovy/com/novoda/staticanalysis/internal/detekt/DetektConfigurator.groovy
+++ plugin/src/main/groovy/com/novoda/staticanalysis/internal/detekt/DetektConfigurator.groovy
@@ -11,9 +11,10 @@ import org.gradle.api.Task
 class DetektConfigurator implements Configurator {
 
     private static final String DETEKT_PLUGIN = 'io.gitlab.arturbosch.detekt'
+    private static final String LAST_COMPATIBLE_DETEKT_VERSION = '1.0.0.RC8'
     private static final String DETEKT_NOT_APPLIED = 'The Detekt plugin is configured but not applied. Please apply the plugin in your build script.\nFor more information see https://github.com/arturbosch/detekt.'
     private static final String OUTPUT_NOT_DEFINED = 'Output not defined! To analyze the results, `output` needs to be defined in Detekt profile.'
-    private static final String DETEKT_CONFIGURATION_ERROR = 'A problem occurred while configuring Detekt. Please make sure to use a compatible version.'
+    private static final String DETEKT_CONFIGURATION_ERROR = "A problem occurred while configuring Detekt. Please make sure to use a compatible version (All versions up to $LAST_COMPATIBLE_DETEKT_VERSION)"
 
     private final Project project
     private final Violations violations
