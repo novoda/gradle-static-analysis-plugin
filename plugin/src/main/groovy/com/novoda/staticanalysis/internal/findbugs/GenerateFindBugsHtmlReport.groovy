87cffb0af33e1040fb7e14035c9791b9fc40d13a
Said Tahsin Dane <tasomaniac@gmail.com>
2018-11-09 23:50:17 +0100

    Add input output and have a test for up-to-date status

--- plugin/src/main/groovy/com/novoda/staticanalysis/internal/findbugs/GenerateFindBugsHtmlReport.groovy
+++ plugin/src/main/groovy/com/novoda/staticanalysis/internal/findbugs/GenerateFindBugsHtmlReport.groovy
@@ -1,19 +1,30 @@
 package com.novoda.staticanalysis.internal.findbugs
 
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.PathSensitive
+
+import static org.gradle.api.tasks.PathSensitivity.RELATIVE
 
 class GenerateFindBugsHtmlReport extends JavaExec {
 
+    @InputFile
+    @PathSensitive(RELATIVE)
     File xmlReportFile
+    @OutputFile
+    @PathSensitive(RELATIVE)
     File htmlReportFile
+
+    GenerateFindBugsHtmlReport() {
+        onlyIf { xmlReportFile?.exists() }
+    }
 
     @Override
     void exec() {
-        if (xmlReportFile?.exists()) {
-            main = 'edu.umd.cs.findbugs.PrintingBugReporter'
-            standardOutput = new FileOutputStream(htmlReportFile)
-            args '-html', xmlReportFile
-            super.exec()
-        }
+        main = 'edu.umd.cs.findbugs.PrintingBugReporter'
+        standardOutput = new FileOutputStream(htmlReportFile)
+        args '-html', xmlReportFile
+        super.exec()
     }
 }
