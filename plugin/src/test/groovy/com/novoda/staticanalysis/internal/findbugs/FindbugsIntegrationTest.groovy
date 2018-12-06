907fd74f131f773546c9736dd8d51119598f1e24
Said Tahsin Dane <tasomaniac@gmail.com>
2018-12-06 19:45:37 +0100

    Revert the changes to the test

--- plugin/src/test/groovy/com/novoda/staticanalysis/internal/findbugs/FindbugsIntegrationTest.groovy
+++ plugin/src/test/groovy/com/novoda/staticanalysis/internal/findbugs/FindbugsIntegrationTest.groovy
@@ -363,7 +363,12 @@ class FindbugsIntegrationTest {
     @Test
     void shouldBeUpToDateWhenCheckTaskRunsAgain() {
         def project = projectRule.newProject()
-                .withSourceSet('main', SOURCES_WITH_LOW_VIOLATION)
+                .withSourceSet('debug', SOURCES_WITH_LOW_VIOLATION, SOURCES_WITH_MEDIUM_VIOLATION)	                .withSourceSet('main', SOURCES_WITH_LOW_VIOLATION)
+                .withSourceSet('release', SOURCES_WITH_HIGH_VIOLATION)	
+                .withPenalty('''{	
+                    maxErrors = 10	
+                    maxWarnings = 10	
+                }''')
                 .withToolsConfig('findbugs {}')
 
         project.build('check')
