80e66bd5870c23a964b8831009f8017ffa69a33b
pablisco <pabs87@gmail.com>
2019-03-06 10:03:35 +0000

    Upgrade gradle android plugin across the board

--- plugin/src/test/groovy/com/novoda/test/TestAndroidKotlinProject.groovy
+++ plugin/src/test/groovy/com/novoda/test/TestAndroidKotlinProject.groovy
@@ -9,7 +9,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.1.4'
+        classpath 'com.android.tools.build:gradle:3.3.2'
         classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20'
     }
 }
